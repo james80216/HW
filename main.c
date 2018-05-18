ecced4a882aafd32bc7f2bb35950ee0874e53c20
james80216 <a0973038325@gmail.com>
2018-05-18 10:08:43 +0800

    HW

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-int i,j;
+int AdultVedio,HomeWork;/*變數宣告*/
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int save();
+int save();/*副程式*/ 
 int main(int argc, char *argv[]) {
-	save();
-	printf("%d",j);
+	save();/*叫出副程式*/ 
+	printf("%d",HomeWork);/*印出值*/ 
 	return 0;
 	
 }
-int save()
+int save()/*宣告副程式*/ 
 {
-	for(i=0;i<10;i++){
+	for(AdultVedio=0;AdultVedio<10;AdultVedio++)/*迴圈*/
+	{
 	
-	j=j+i;	
+	HomeWork=HomeWork+AdultVedio;/*計算*/	
 }
 }
